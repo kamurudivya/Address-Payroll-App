506f5da4abbc156971c91d46bf008ee961eeb19d
kamuru divya <kamuru.divya8@gmail.com>
2022-04-09 08:23:11 +0530

    Refector: app.js to launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo

--- helloworld/helloworld/src/App.js
+++ helloworld/helloworld/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import React from 'react';
 import logo from './Bl logo.png';
-
 class App extends React.Component {
-
+  url = 'https://www.bridgelabz.com/'
   constructor() {
     super()
     this.state = {
       title: 'hello from bridgelabz'
     }
   }
+
+  // onClick function
+  onClick = ($event) => {
+    console.log('save button is clicked', $event);
+    window.open(this.url, "_blank");
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo}
+        <img src={logo} onClick={this.onClick}
           alt="This Bridgelabz logo: a Bridge to Employment through lab works" />
       </div>
     );
