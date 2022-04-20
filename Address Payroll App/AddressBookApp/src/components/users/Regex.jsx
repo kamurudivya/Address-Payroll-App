4cf7cd4bd5e116b8584318ace651cb9ae8c96136
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2022-04-20 22:19:17 +0530

    Delete Address Payroll App/AddressBookApp directory

--- Address Payroll App/AddressBookApp/src/components/users/Regex.jsx
+++
@@ -1,5 +0,0 @@
-// export const validFullName = new RegExp('^[A-Z]{1}[a-z]{2,}$');
-//  export const validZipcode = new RegExp
-//     ('/^\d{6}(?:[- ]?\d{5})?$/');
-//  export const validPhoneNumber = new RegExp('^[+][1-9]{2}[-][0-9]{10}$');
-
